89fba6d7d2e88ac1df162a6a754528041c5c0a2e
Davin Ryo Tanada <ryotanada@yahoo.co.id>
2024-12-11 23:10:04 +0800

    New JS and images

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from './logo.jpg';
+
+const Header = () => {
+return(
+    <img src={logo} alt="Logo" />
+);
+};
+
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+return(
+    <>
+        <ul>
+            <li><a href="/home"> Home </a></li>
+            <li><a href="/menu"> Menu </a></li>
+            <li><a href="/contact"> Contact Us </a></li>
+        </ul>
+    </>
+);
+};
+
+export default Nav;
